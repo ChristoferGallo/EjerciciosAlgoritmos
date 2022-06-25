6aa55d89c97cbfd642591b5adc2b85d58646a3fe
ChristoferGallo <christofer.gallo.gtz@gmail.com>
2022-06-24 19:19:15 -0500

    Se agrega el primer programa

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # EjerciciosAlgoritmos
-Se realizan los programas de los ejercicios de algoritmos con Javascript
+Realización de los programas de los ejercicios de algoritmos con Javascript
+## Se comienza con la realización de los programas
+1. Crear un programa que solicite 3 números enteros entre el 1 y el 100 y definir cual es el número mayor
+2. Crear un programa que solicite 3 números enteros entre el 1 y el 100 y definir cual es el número menor
+3. Crear un programa que adivine un número entero entre el 1 y el 100 en el menor número de pasos posibles
+4. Crear un programa que solicite un número entero entre el 1 y el 100 y determinar si es multiplo de 3
+5. Crear un programa que lea 3 números y determinar si uno es la suma de los otros dos
+6. Crear un programa que lea un número y determinar si es par o impar 
+
+----
+
+> Nota: El programa #1 se encuentra en desarrollo
---
+++ js/1er_Algoritmo.js
@@ -0,0 +1,27 @@
+let Mayor = () => {
+    n1 = parseInt(prompt("Ingresa el primer valor"));
+    if(n1 >= 1 && n1 <= 100){
+        console.log("El primer valor ingresado esta dentro del rango");
+        n2 = parseInt(prompt("Ingresa el segundo valor"));
+        if(n2 >= 1 && n2 <= 100) {
+            n3 = parseInt(prompt("Ingresa el tercer valor"));
+            console.log("El segundo valor ingresado esta dentro del rango");
+            if(n3 >= 1 && n3 <= 100) {
+                console.log("El tercer valor ingresado esta dentro del rango")
+                if(n1 > n2 && n1 > n3){
+                    console.log("El primer valor ingresado es el mayor")
+                }
+                
+                if(n2 > n1 && n2 > n3){
+                    console.log("El segundo valor ingresado es el mayor")
+                }
+                
+                if(n3 > n1 && n3 > n1){
+                    console.log("El tercer valor ingresado es el mayor")
+                }
+            }
+        }
+    }
+} 
+
+Mayor();
